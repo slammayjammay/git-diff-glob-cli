ae5618837939c3612bddbe575c1629fb11438d97
Scott McKenzie <slammayjammay@gmail.com.com>
2017-01-12 16:06:35 -0800

    Helper script to create help screen

---
+++ helpfile.js
@@ -0,0 +1,23 @@
+const execSync = require('child_process').execSync
+const chalk = require('chalk')
+
+const outputFile = 'help.txt'
+let text = `\
+${chalk.bold('DESCRIPTION')}
+  Print diffs for individual or multiple files. Provide substrings
+  of files you want the diff of. All files matching the given
+  substrings will be diff-ed and printed to the screen.
+
+${chalk.bold('USAGE')}
+  gd [options] [path...]
+
+${chalk.bold('OPTIONS')}
+  -h, --help          Display this help screen.
+  -n, --no-pager      Log output onto main screen.
+
+${chalk.bold('EXAMPLES')}
+  gd html
+  gd --no-pager json
+`
+
+execSync(`echo "${text}" > ${outputFile}`)
