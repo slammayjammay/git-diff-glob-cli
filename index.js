bfaf781f4c0d792e4d80710fb5d13a3a3d7e8000
Scott McKenzie <slammayjammay@gmail.com.com>
2017-01-12 16:00:03 -0800

    Show help screen

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const join = require('path').join
 const execSync = require('child_process').execSync
 const spawnSync = require('child_process').spawnSync
+const chalk = require('chalk')
 const gitDiffGlob = require('git-diff-glob')
 
 class GitDiffGlobCli {
 	constructor(options, args) {
-		console.log(options)
+		if (options.help) {
+			this.showHelpScreen()
+			return
+		}
 
-		this.options = options
 		let diff = gitDiffGlob(args)
 
 		if (options.pager) {
@@ -24,6 +27,12 @@ class GitDiffGlobCli {
 	outputToPager(diff) {
 		let pager = join(__dirname, './helpers/pager')
 		spawnSync(`${pager}`, [diff], { stdio: 'inherit' })
+	}
+
+	showHelpScreen() {
+		let helpfile = './help.txt'
+		let help = execSync(`cat ${helpfile}`).toString('utf8')
+		console.log(help)
 	}
 }
 
