86d699025eb474d7dab93c71d9d86be068fd84ff
Scott McKenzie <slammayjammay@gmail.com>
2017-01-14 20:41:52 -0800

    Add case-sensitive option

--- helpfile.js
+++ helpfile.js
@@ -6,14 +6,16 @@ let text = `\
 ${chalk.bold('DESCRIPTION')}
   Print diffs for individual or multiple files. Provide substrings
   of files you want the diff of. All files matching the given
-  substrings will be diff-ed and printed to the screen.
+  substrings will be diff-ed and printed to the screen, by default
+  case-insensitive.
 
 ${chalk.bold('USAGE')}
   gd [options] [path...]
 
 ${chalk.bold('OPTIONS')}
-  -h, --help          Display this help screen.
-  -n, --no-pager      Log output onto main screen.
+  -c, --case-sensitive    Match substring case-sensitively.
+  -h, --help              Display this help screen.
+  -n, --no-pager          Log output onto main screen.
 
 ${chalk.bold('EXAMPLES')}
   gd html
